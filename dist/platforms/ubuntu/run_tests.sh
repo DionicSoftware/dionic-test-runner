b9fd1899387863548a038f29717b26e18dedd9aa
Sepehr Razmyar <69770592+Null3rror@users.noreply.github.com>
2025-06-03 10:13:33 +0200

    Update run_tests.sh

--- dist/platforms/ubuntu/run_tests.sh
+++ dist/platforms/ubuntu/run_tests.sh
@@ -196,7 +196,7 @@ for platform in ${TEST_PLATFORMS//;/ }; do
   echo ""
 
   if [[ "$platform" == "customstandalone" ]]; then
-    runTests="--run-tests --path-to-old-saves $UNITY_PROJECT_PATH/ExplorersTestSaves/"
+    runTests="--run-tests --path-to-old-saves $UNITY_PROJECT_PATH/HavenCraftTestSaves/"
   else
     runTests="-executeMethod TestModule.RunTestsByScript"
   fi
